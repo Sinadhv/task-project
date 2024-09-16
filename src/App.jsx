442bf8ca1493fbed345e33556d72638ff4b379c4
Sina Dehvari <cnadhv7@gmail.com>
2024-09-16 14:56:33 +0330

    create project
    create a project with vite react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import AppHeader from './components/AppHeader';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+
+
+const App = () => {
+  return (
+   <>
+        <Navbar />
+        <Sidebar/>
+        <AppHeader/>
+        </>
+  )
+};
+
+export default App;
